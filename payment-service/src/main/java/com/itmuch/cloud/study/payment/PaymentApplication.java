a70f531721097fe605c4fb58129bf6ed6b1f9383
谢荣雳 <xrl6238@arcsoft.com.cn>
2019-09-15 20:41:55 +0800

    易宝支付相关接口

--- payment-service/src/main/java/com/itmuch/cloud/study/payment/PaymentApplication.java
+++ payment-service/src/main/java/com/itmuch/cloud/study/payment/PaymentApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableSwagger2Doc
