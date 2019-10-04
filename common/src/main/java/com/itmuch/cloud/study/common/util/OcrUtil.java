8382d0860a54813638cec738cb4f694d0c71080b
谢荣雳 <xrl6238@arcsoft.com.cn>
2019-10-04 16:15:36 +0800

    百度OCR工具类

--- common/src/main/java/com/itmuch/cloud/study/common/util/OcrUtil.java
+++ common/src/main/java/com/itmuch/cloud/study/common/util/OcrUtil.java
@@ -1,6 +1,7 @@
 package com.itmuch.cloud.study.common.util;
 
 import com.baidu.aip.ocr.AipOcr;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.util.ResourceUtils;
@@ -12,7 +13,7 @@ import java.util.Properties;
 
 public class OcrUtil {
     //设置APPID/AK/SK
-    public static final String APP_ID =  ConfigUtil.BAIDU_OCR_APP_ID;
+    public static final String APP_ID = ConfigUtil.BAIDU_OCR_APP_ID;
     public static final String API_KEY = ConfigUtil.BAIDU_OCR_APP_KEY;
     public static final String SECRET_KEY = ConfigUtil.BAIDU_OCR_SECRET_KEY;
 
@@ -27,24 +28,28 @@ public class OcrUtil {
 
     /**
      * 通用图片识别
-     * @param client
+     *
+     * @param path 图片路径
      */
-    public static void basicImageOcr(AipOcr client)  {
-        String path = null;
+    public static String basicImageOcr(String path) {
+        path = "classpath:static/" + path;
         try {
-            path = ResourceUtils.getFile("classpath:static/images/test.png").getAbsolutePath();
-            JSONObject res = client.basicGeneral(path, new HashMap<String, String>());
-            System.out.println(res.toString());
+            path = ResourceUtils.getFile(path).getAbsolutePath();
+            JSONObject res = apiOcr.basicGeneral(path, new HashMap<String, String>());
+            return res.toString();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return StringUtils.EMPTY;
     }
 
     /**
      * 身份证识别
-     * @param client
+     *
+     * @param imageName 图片路径
+     * @param cardSide  front 正面 back反面
      */
-    public static void identityOcr(AipOcr client){
+    public static String identityOcr(String imageName, String cardSide) {
         // 参数为本地图片路径
         String image = null;
         try {
@@ -52,38 +57,43 @@ public class OcrUtil {
             HashMap<String, String> options = new HashMap<String, String>();
             options.put("detect_direction", "true");
             options.put("detect_risk", "false");
-            String idCardSide = "front";
-            image = ResourceUtils.getFile("classpath:static/images/test.jpg").getAbsolutePath();
-            JSONObject res = client.idcard(image, idCardSide, options);
-            System.out.println(res.toString());
+            String idCardSide = cardSide;
+            image = ResourceUtils.getFile("classpath:static/" + imageName).getAbsolutePath();
+            JSONObject res = apiOcr.idcard(image, idCardSide, options);
+            return res.toString();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return StringUtils.EMPTY;
     }
 
     /**
      * 银行卡识别
-     * @param client
+     *
+     * @param imageName 图片名称
      */
-    public static void bankCardOcr(AipOcr client){
+    public static String bankCardOcr(String imageName) {
         // 传入可选参数调用接口
         HashMap<String, String> options = new HashMap<String, String>();
         // 参数为本地图片路径
         String image = null;
         try {
-            image = ResourceUtils.getFile("classpath:static/images/bank.jpg").getAbsolutePath();
+            image = ResourceUtils.getFile("classpath:static/" + imageName).getAbsolutePath();
+            JSONObject res = apiOcr.bankcard(image, options);
+            return res.toString();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        JSONObject res = client.bankcard(image, options);
-        System.out.println(res.toString());
+
+        return StringUtils.EMPTY;
     }
 
     /**
      * 驾照识别
+     *
      * @param client
      */
-    public static void drivingLicenseOcr(AipOcr client){
+    public static void drivingLicenseOcr(AipOcr client) {
         // 传入可选参数调用接口
         HashMap<String, String> options = new HashMap<String, String>();
         options.put("detect_direction", "true");
@@ -102,39 +112,39 @@ public class OcrUtil {
 
     /**
      * 营业执照识别
-     * @param apiOcr
+     *
      * @param path
      * @return
      */
-    public static Map<String,Object> parseLicence(AipOcr apiOcr, String path) {
-        Map<String,Object> result = new HashMap<>();
-        try{
+    public static Map<String, Object> parseLicence(String path) {
+        Map<String, Object> result = new HashMap<>();
+        try {
             JSONObject res2 = apiOcr.basicGeneral(path, new HashMap<String, String>());
             JSONArray res = (JSONArray) res2.get("words_result");
 
             if (res.length() > 0) {
                 for (int key = 0; key < res.length(); key++) {
-                    result.put("code","0");
+                    result.put("code", "0");
                     JSONObject object = (JSONObject) res.get(key);
                     String value = object.getString("words");
-                    if(value.startsWith("法定代表人")){
-                        result.put("name",value.substring(5,value.length()));
+                    if (value.startsWith("法定代表人")) {
+                        result.put("name", value.substring(5, value.length()));
                     }
-                    if(value.trim().startsWith("名称")){
-                        result.put("companyName",value.substring(2,value.length()));
-                    }else if(value.trim().startsWith("称")){
-                        result.put("companyName",value.substring(1,value.length()));
+                    if (value.trim().startsWith("名称")) {
+                        result.put("companyName", value.substring(2, value.length()));
+                    } else if (value.trim().startsWith("称")) {
+                        result.put("companyName", value.substring(1, value.length()));
                     }
-                    if(value.startsWith("统一社会信用代码")){
-                        result.put("creditCode",value.substring(8,value.length()));
+                    if (value.startsWith("统一社会信用代码")) {
+                        result.put("creditCode", value.substring(8, value.length()));
                     }
                 }
                 return result;
-            }else{
-                result.put("code","-1");
-                result.put("creditCode","");
+            } else {
+                result.put("code", "-1");
+                result.put("creditCode", "");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
         return result;
---
+++ common/src/test/java/com/itmuch/cloud/study/common/test/OcrUtilTest.java
@@ -0,0 +1,36 @@
+package com.itmuch.cloud.study.common.test;
+
+import com.itmuch.cloud.study.common.util.OcrUtil;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileNotFoundException;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * Description: 百度识别工具类
+ *
+ * @author xierongli
+ * @create 2019-10-04 15:51
+ */
+public class OcrUtilTest {
+
+
+    public static void main(String[] args) {
+
+        String basicImagePath = "test.png";
+        System.out.println(OcrUtil.basicImageOcr(basicImagePath));
+        System.out.println(OcrUtil.identityOcr("a-fromt.jpeg", "front"));
+        System.out.println(OcrUtil.identityOcr("a-back.jpeg", "back"));
+//        System.out.println(OcrUtil.bankCardOcr("bank.png"));
+
+        try {
+            String path  = ResourceUtils.getFile("classpath:static/business.png").getAbsolutePath();
+            System.out.println(OcrUtil.parseLicence(path));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
